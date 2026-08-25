e5b99ce593f0cbe3bead00665971477a4878dfe6
Nahuel Geno <142752140+NahuelGeno@users.noreply.github.com>
2026-08-24 22:18:09 -0300

    Update games.js

--- games.js
+++ games.js
@@ -1,10 +1,13 @@
 // games.js
 const catalogoJuegos = [
-  { titulo: "Super Mario World", anio: 1990, puntaje: 88, genero: "Plataformas" },
-  { titulo: "DOOM", anio: 1993, puntaje: 92, genero: "FPS" },
-  { titulo: "Unreal Tournament", anio: 1999, puntaje: 97, genero: "Arena Shooter" },
-  { titulo: "Half-Life 2", anio: 2004, puntaje: 102, genero: "FPS" },
-  { titulo: "Portal 2", anio: 2011, puntaje: 108, genero: "Puzzle" },
-  { titulo: "Elden Ring", anio: 2022, puntaje: 118, genero: "ARPG" },
-  { titulo: "Metaphor: ReFantazio", anio: 2024, puntaje: 116, genero: "JRPG" }
+  { titulo: "Super Mario World", anio: 1990, puntaje: 88, plataforma: "SNES", genero: "Plataformas" },
+  { titulo: "DOOM", anio: 1993, puntaje: 92, plataforma: "PC", genero: "FPS" },
+  { titulo: "Unreal Tournament", anio: 1999, puntaje: 97, plataforma: "PC", genero: "Arena Shooter" },
+  { titulo: "The Legend of Zelda: Ocarina of Time", anio: 1998, puntaje: 97, plataforma: "Nintendo 64", genero: "Aventura" },
+  { titulo: "Half-Life 2", anio: 2004, puntaje: 102, plataforma: "PC", genero: "FPS" },
+  { titulo: "Shadow of the Colossus", anio: 2005, puntaje: 101, plataforma: "PlayStation 2", genero: "Aventura" },
+  { titulo: "Halo 3", anio: 2007, puntaje: 101, plataforma: "Xbox 360", genero: "FPS" },
+  { titulo: "Portal 2", anio: 2011, puntaje: 108, plataforma: "PC", genero: "Puzzle" },
+  { titulo: "Elden Ring", anio: 2022, puntaje: 118, plataforma: "Multiplataforma", genero: "ARPG" },
+  { titulo: "Metaphor: ReFantazio", anio: 2024, puntaje: 116, plataforma: "Multiplataforma", genero: "JRPG" }
 ];
