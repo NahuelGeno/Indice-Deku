bad18c253a3ece6562356969a8ae4d98d62da62a
Nahuel Geno <142752140+NahuelGeno@users.noreply.github.com>
2026-08-24 22:02:47 -0300

    Create games.js

---
+++ games.js
@@ -0,0 +1,10 @@
+// games.js
+const catalogoJuegos = [
+  { titulo: "Super Mario World", anio: 1990, puntaje: 88, genero: "Plataformas" },
+  { titulo: "DOOM", anio: 1993, puntaje: 92, genero: "FPS" },
+  { titulo: "Unreal Tournament", anio: 1999, puntaje: 97, genero: "Arena Shooter" },
+  { titulo: "Half-Life 2", anio: 2004, puntaje: 102, genero: "FPS" },
+  { titulo: "Portal 2", anio: 2011, puntaje: 108, genero: "Puzzle" },
+  { titulo: "Elden Ring", anio: 2022, puntaje: 118, genero: "ARPG" },
+  { titulo: "Metaphor: ReFantazio", anio: 2024, puntaje: 116, genero: "JRPG" }
+];
